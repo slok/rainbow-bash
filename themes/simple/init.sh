743a1ffebc18d334c31fbb6c427b35d9d011e8cc
Xabier Larrakoetxea <slok69@gmail.com>
2015-01-27 13:24:29 +0100

    Fixed simple error

--- themes/simple/init.sh
+++ themes/simple/init.sh
@@ -1,10 +1,24 @@
 
-get_git_fmt(){
+# This is a hack for the colors. Colors need to be between \[\],
+# if you return from "get_git_info" \[$RBW_BR_RED\]$rbw_git_branch  then
+# Bash can't interpret those brackets and will cause problems
+
+get_git_color(){
     if [[ $rbw_git_is_repo == 1 ]]; then
         if [[ $rbw_git_dirty == 1 ]]; then
-            echo -en "$RBW_BR_RED $rbw_git_branch "
+            echo -en "$RBW_BR_RED"
         else
-            echo -en "$RBW_BR_GREEN $rbw_git_branch "
+            echo -en "$RBW_BR_GREEN"
+        fi
+    fi
+}
+
+get_git_info(){
+    if [[ $rbw_git_is_repo == 1 ]]; then
+        if [[ $rbw_git_dirty == 1 ]]; then
+            echo -en "$rbw_git_branch  "
+        else
+            echo -en "$rbw_git_branch  "
         fi
     fi
 }
@@ -12,6 +26,6 @@ get_git_fmt(){
 RBW_THEME_SIMPLE=" \
 \[$RBW_BR_RED\]\357\204\270  \
 \[$RBW_BR_CYAN\]\W\
-\$(get_git_fmt)\
-\[$RBW_BR_CYAN\] :\
+ \[\$(get_git_color)\]\$(get_git_info)\
+\[$RBW_BR_CYAN\]:\
 \[$RBW_RESET_ALL\] "
